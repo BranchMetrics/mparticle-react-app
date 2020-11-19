2be289b8e67ada0d53a712520f1dc40fd60d99cc
Jimmy Dee <jimmy.dee@branch.io>
2020-11-18 21:11:08 -0800

    Use function-based component instead of class

--- src/App.js
+++ src/App.js
@@ -4,33 +4,31 @@ import React from 'react';
 import './mparticle'; // defines window.mParticle
 import 'branch-mparticle-web-kit';
 
-class App extends React.Component {
-  render() {
-    const closeJourney = () => {
-      window.branch.closeJourney((err, data) => {
-        console.log(`error: ${err}, data: ${JSON.stringify(data)}`)
-      });
-    };
-    const trackPageview = () => {
-      window.branch.track('pageview');
-    };
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <button onClick={closeJourney}>
-            Close Journey
-          </button>
-          <button onClick={trackPageview}>
-            Track Pageview
-          </button>
-        </header>
-      </div>
-    );
-  }
+function App() {
+  const closeJourney = () => {
+    window.branch.closeJourney((err, data) => {
+      console.log(`error: ${err}, data: ${JSON.stringify(data)}`)
+    });
+  };
+  const trackPageview = () => {
+    window.branch.track('pageview');
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={closeJourney}>
+          Close Journey
+        </button>
+        <button onClick={trackPageview}>
+          Track Pageview
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default App;
