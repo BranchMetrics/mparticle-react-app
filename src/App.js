55d93e4af981e24ef6f0eb553a205d57a595271c
Jimmy Dee <jimmy.dee@branch.io>
2020-11-18 20:43:44 -0800

    Add buttons to close the Journey and track a pageview to reopen it.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import './mparticle'; // defines window.mParticle
+import 'branch-mparticle-web-kit';
 
 class App extends React.Component {
   render() {
+    const closeJourney = () => {
+      window.branch.closeJourney((err, data) => {
+        console.log(`error: ${err}, data: ${JSON.stringify(data)}`)
+      });
+    };
+    const trackPageview = () => {
+      window.branch.track('pageview');
+    };
     return (
       <div className="App">
         <header className="App-header">
@@ -12,14 +21,12 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={closeJourney}>
+            Close Journey
+          </button>
+          <button onClick={trackPageview}>
+            Track Pageview
+          </button>
         </header>
       </div>
     );
