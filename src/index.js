063b09d022632eecb71acf89c2620cb9834b32e8
Jimmy Dee <jimmy.dee@branch.io>
2020-11-19 20:46:07 -0800

    Rewrite initialization code

--- src/index.js
+++ src/index.js
@@ -3,31 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'branch-mparticle-web-kit';
+import mParticle from '@mparticle/web-sdk';
+import BranchMetrics from 'branch-mparticle-web-kit';
 
-window.mParticle = {
-     config: {
-         isDevelopmentMode: true, //switch to false (or remove) for production
-         identifyRequest: {
-             userIdentities: {
-                 other3: 'otheridentity3',
-                 customerid: '123456'
-             }
-         },
-         identityCallback: function(result) {
-             // You can check if there's a populated user object, otherwise there was an error
-             // You can also inspect the result.httpCode - see below for a description of the supported codes
-             if (result.getUser()) {
-                 result.getUser().setUserAttribute('age', '25');
-             } else {
-                 //the IDSync call failed - see below for more details on failed requests
-             }
-         }
-     }
-};
-
-(function(t){window.mParticle=window.mParticle||{};window.mParticle.EventType={Unknown:0,Navigation:1,Location:2,Search:3,Transaction:4,UserContent:5,UserPreference:6,Social:7,Other:8};window.mParticle.eCommerce={Cart:{}};window.mParticle.Identity={};window.mParticle.config=window.mParticle.config||{};window.mParticle.config.rq=[];window.mParticle.config.snippetVersion=2.2;window.mParticle.ready=function(t){window.mParticle.config.rq.push(t)};var e=["endSession","logError","logBaseEvent","logEvent","logForm","logLink","logPageView","setSessionAttribute","setAppName","setAppVersion","setOptOut","setPosition","startNewSession","startTrackingLocation","stopTrackingLocation"];var o=["setCurrencyCode","logCheckout"];var i=["identify","login","logout","modify"];e.forEach(function(t){window.mParticle[t]=n(t)});o.forEach(function(t){window.mParticle.eCommerce[t]=n(t,"eCommerce")});i.forEach(function(t){window.mParticle.Identity[t]=n(t,"Identity")});function n(e,o){return function(){if(o){e=o+"."+e}var t=Array.prototype.slice.call(arguments);t.unshift(e);window.mParticle.config.rq.push(t)}}var mp=document.createElement("script");mp.type="text/javascript";mp.async=true;mp.src=("https:"==document.location.protocol?"https://jssdkcdns":"http://jssdkcdn")+".mparticle.com/js/v2/"+t+"/mparticle.js";var c=document.getElementsByTagName("script")[0];c.parentNode.insertBefore(mp,c)}
-)("ad0494840f664c488a4c924d97ae5fdf");
+const config = {};
+BranchMetrics.register(config);
+mParticle.init("ad0494840f664c488a4c924d97ae5fdf", config);
 
 ReactDOM.render(
   <React.StrictMode>
