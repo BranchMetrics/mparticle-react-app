43100aa48b1e1acba1069dbb13ff6e8c2aae6268
Jimmy Dee <jimmy.dee@branch.io>
2020-11-19 22:48:48 -0800

    Journey on each page

--- src/About.js
+++ src/About.js
@@ -1,7 +1,33 @@
 import { Component } from 'react';
 import './App.css';
+import 'branch-mparticle-web-kit';
 
 export default class About extends Component {
+  componentDidMount() {
+    if (!window.branch) {
+      return;
+    }
+
+    window.branch.track('pageview', (err, data) => {
+      console.log(`pageview error ${err}, response ${JSON.stringify(data)}`);
+    });
+  }
+
+  componentWillUnmount() {
+    if (!window.branch) {
+      return;
+    }
+
+    window.branch.closeJourney(err => {
+      if (err) {
+        console.warn(`closeJourney error ${err}`);
+      }
+      else {
+        console.log('closeJourney succeeded');
+      }
+    });
+  }
+
   render() {
     return (
       <h2>About</h2>
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,33 @@
 import { Component } from 'react';
 import './App.css';
+import 'branch-mparticle-web-kit';
 
 export default class Home extends Component {
+  componentDidMount() {
+    if (!window.branch) {
+      return;
+    }
+
+    window.branch.track('pageview', (err, data) => {
+      console.log(`pageview error ${err}, response ${JSON.stringify(data)}`);
+    });
+  }
+
+  componentWillUnmount() {
+    if (!window.branch) {
+      return;
+    }
+
+    window.branch.closeJourney(err => {
+      if (err) {
+        console.warn(`closeJourney error ${err}`);
+      }
+      else {
+        console.log('closeJourney succeeded');
+      }
+    });
+  }
+
   render() {
     return (
       <h2>Home</h2>
